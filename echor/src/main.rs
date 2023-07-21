56e2cc2b5c3b03556a2752206f8d2eb76662bf6c
Xuan <97ssps30212@gmail.com>
2023-07-21 18:46:21 +0800

    refactor: align the deps version to the book

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,31 +1,27 @@
-use clap::{Arg, ArgAction, Command};
+use clap::{App, Arg};
 
 fn main() {
-    let matches = Command::new("echor")
+    let matches = App::new("echor")
         .version("0.1.0")
         .author("Yu Xuan")
         .about("Rust echo")
         .arg(
-            Arg::new("text")
+            Arg::with_name("text")
                 .value_name("TEXT")
                 .help("Input text")
                 .required(true)
-                .num_args(1..),
+                .min_values(1),
         )
         .arg(
-            Arg::new("omit_newline")
-                .short('n')
+            Arg::with_name("omit_newline")
+                .short("n")
                 .help("Do not print newline")
-                .action(ArgAction::SetTrue),
+                .takes_value(false),
         )
         .get_matches();
 
-    let text: Vec<&str> = matches
-        .get_many::<String>("text")
-        .unwrap_or_default()
-        .map(|v| v.as_str())
-        .collect();
-    let omit_newline = *matches.get_one::<bool>("omit_newline").unwrap_or(&false);
+    let text = matches.values_of_lossy("text").unwrap();
+    let omit_newline = matches.is_present("omit_newline");
     let ending = if omit_newline { "" } else { "\n" };
 
     print!("{}{}", text.join(" "), ending)
